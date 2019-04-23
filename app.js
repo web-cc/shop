50d9c866efb12d70827c0dae7667ce996826a507
PhongLe <phongcoi696@gmail.com>
2019-04-23 17:49:43 +0700

    add index in app.js

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cart',cartRouter);
 app.use('/category',categoryRouter);
