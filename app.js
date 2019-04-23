8899486cc07f5828757779ec56a587a44d47585c
PhongLe <phongcoi696@gmail.com>
2019-04-23 11:21:06 +0700

    add Router in app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cartRouter = require('./routes/cart');
+var categoryRouter = require('./routes/category');
+var checkoutRouter = require('./routes/checkout');
+var loginRouter = require('./routes/login');
+var singleProductRouter = require('./routes/single-product');
 
 var app = express();
 
@@ -23,6 +27,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cart',cartRouter);
+app.use('/category',categoryRouter);
+app.use('/checkout',checkoutRouter);
+app.use('/login',loginRouter);
+app.use('/single-product',singleProductRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
