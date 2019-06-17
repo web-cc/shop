25e36eb2b72f3ec7242f2b35d7dbdc532336f3f1
PhongLeDev <43945680+PhongLeDev@users.noreply.github.com>
2019-06-17 10:41:24 +0700

    update orderDetail.js

--- model/orderDetail.js
+++ model/orderDetail.js
@@ -1,6 +1,5 @@
-var orderDetail = function (id, employee_id, order_id, product_id, price, status) {
+var orderDetail = function (id, order_id, product_id, price, status) {
     this.id = id;
-    this.employee_id = employee_id;
     this.order_id = order_id;
     this.product_id=product_id;
     this.price = price;
